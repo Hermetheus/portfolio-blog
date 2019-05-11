0a064488b5171086a49c1e02b16505062ab8de54
Allan Olson <allanmolson@gmail.com>
2019-05-11 10:34:58 -0500

    Little Things again!

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 <!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
 <p align="center">
   <a href="https://www.gatsbyjs.org">
-    <img alt="Gatsby" src="https://www.gatsbyjs.org/monogram.svg" width="60" />
+    <img alt="Allan Wakefield-Olson" src="/src/components/images/logo.png" width="60" />
   </a>
 </p>
 <h1 align="center">
 Allan Wakefield-Olson's Portfolio
 </h1>
-
+<div align="center">
+<a href="https://www.allanmolson.com">www.allanmolson.com</a>
+</div>
 <hr />
+<ul>
 TODO LIST:
+<li>
 FIX IMAGES
+</li>
+<li>
 ADD IMAGES TO BLOG POSTS
+</li>
+<li>
 MAKE ABOUT ME & MAIN PAGE
+</li>
+</ul>
+
 <!-- AUTO-GENERATED-CONTENT:END -->
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import React from "react"
 import Head from "../components/head"
 import Layout from "../components/layout"
-import "../styles/index.scss"
+import indexStyles from "./index.module.scss"
 
 const IndexPage = () => {
   return (
@@ -11,7 +11,9 @@ const IndexPage = () => {
       <Head />
       <div class="container">
         <div class="prime">
-          <h1 class="title">Hello, my name is Allan Wakefield-Olson</h1>
+          <h1 className={indexStyles.title}>
+            Hello, my name is Allan Wakefield-Olson
+          </h1>
           <p class="about">
             I am currently a <b>Branch Operations Manager</b> that is self
             teaching how to code. I am extremely passionate about becoming a
@@ -23,83 +25,6 @@ const IndexPage = () => {
           <Link to="/about" class="button">
             Learn more
           </Link>
-          <button
-            type="button"
-            class="btn btn-primary"
-            data-toggle="modal"
-            data-target="#Modal"
-            data-whatever="@mdo"
-          >
-            Contact Me
-          </button>
-
-          <div
-            class="modal fade"
-            id="Modal"
-            tabindex="-1"
-            role="dialog"
-            aria-labelledby="Contact Me"
-            aria-hidden="true"
-          >
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="Modal">
-                    New message
-                  </h5>
-                  <button
-                    type="button"
-                    class="close"
-                    data-dismiss="modal"
-                    aria-label="Close"
-                  >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  <form>
-                    <div class="form-group">
-                      <label for="recipient-name" class="col-form-label">
-                        Recipient:
-                      </label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        id="recipient-name"
-                      />
-                    </div>
-                    <div class="form-group">
-                      <label for="message-text" class="col-form-label">
-                        Message:
-                      </label>
-                      <textarea class="form-control" id="message-text" />
-                    </div>
-                  </form>
-                </div>
-                <div class="modal-footer">
-                  <button
-                    type="button"
-                    class="btn btn-secondary"
-                    data-dismiss="modal"
-                  >
-                    Close
-                  </button>
-                  <button type="button" class="btn btn-primary">
-                    Send message
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <h6>
-            <a
-              href="mailto:allanmolson@gmail.com"
-              class="hvr-grow"
-              title="Email"
-            >
-              <ion-icon name="mail" />
-            </a>
-          </h6>
         </div>
       </div>
     </Layout>
